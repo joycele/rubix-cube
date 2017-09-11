c4128d5f9cb2c9a7d901a4099ecf59560d6c9b93
Joyce Le <jle7798@gmail.com>
2017-09-11 05:00:34 -0700

    Outline for the RubixCube class

---
+++ rubixcube/solve_top.py
@@ -0,0 +1,30 @@
+'''
+Created on Sep 8, 2017
+
+@author: Joyce
+'''
+from moves import Moves
+
+
+class RubixCube:
+    
+    def __init__(self, cube_config: dict):
+        '''Initialize the Rubik's cube to contain the current configuration of the cube'''
+        self.cube_config = cube_config
+    
+    def SolveTop(self, cube_config) -> dict:
+        pass
+    
+    def SolveMiddle(self, cube_config) -> dict:
+        pass
+    
+    def SolveCube(self, cube_config) -> dict:
+        pass
+    
+    def CheckCube(self, cube_config) -> dict:
+        pass
+    
+    
+    
+    
+        
